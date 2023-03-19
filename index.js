7126fa1aef3e42a2ae5dcc0f65f3013129469c4f
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-03-19 23:13:02 +0430

    Add action

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const BUY_CAKE = 'BUY_CAKE'
     type: BUY_CAKE,
     info: 'First redux action'
 }
+
+function buyCake() {
+    return {
+        type: BUY_CAKE,
+        info: 'First redux action'
+    }
+}
