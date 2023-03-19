3c75f40ea067041f49d3b4c468a5939f6ac4b0a0
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-03-20 01:09:56 +0430

    Add Reducer for icecream

--- index.js
+++ index.js
@@ -40,12 +40,6 @@ const reducer = (state = initialState, action) => {
         case BUY_ICECREAM: return {
             ...state,
             numOfIceCreams: state.numOfIceCreams - 1
-        default: return state
-    }
-    switch(action.type) {
-        case BUY_ICECREAM: return {
-            ...state,
-            numOfIceCreams: state.numOfIceCreams - 1
         }
         default: return state
     }
