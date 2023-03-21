fa0647ed4bf1903692e787bd2aba24cb42164e93
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-03-21 20:05:56 +0430

    Add Action creators

--- asyncActions.js
+++ asyncActions.js
@@ -7,3 +7,5 @@ const initialState = {
 const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST'
 const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS'
 const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE'
+
+const fetchUsersRequest = () => {}
