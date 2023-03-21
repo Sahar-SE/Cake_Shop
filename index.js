5eef7a991f2c97f98632564c4814c9df96dde317
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-03-21 19:17:42 +0430

    Import the Middleware of redux-logger

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const redux = require('redux')
-const createStore = redux.createStore
 const reduxLogger = require('redux-logger')
-const logger = reduxLogger.createLogger()
+const createStore = redux.createStore
 const combineReducers = redux.combineReducers
+const logger = reduxLogger.createLogger()
 
 
 const BUY_CAKE = 'BUY_CAKE'
