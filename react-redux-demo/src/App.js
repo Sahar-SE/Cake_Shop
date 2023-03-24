0821b1dca654786a15435c25781d046dbc54808d
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-03-24 21:38:13 +0430

    add CakeContainer in App.js

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import CakeContainer from './components/CakeContainer';
 
 function App() {
   return (
     <div className="App">
-
+      <CakeContainer />
     </div>
   );
 }
