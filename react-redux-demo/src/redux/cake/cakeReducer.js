66a07dd5c2471ce7d2651fa91a369098b423646c
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-03-25 22:27:01 +0430

    Add cakeReducer

--- react-redux-demo/src/redux/cake/cakeReducer.js
+++ react-redux-demo/src/redux/cake/cakeReducer.js
@@ -9,6 +9,7 @@ const cakeReducer = (state = initialState, action) => {
         case BUY_CAKE: return {
             ...state,
             numOfCakes: state.numOfCakes - 1
-        } 
+        }
+        default: return state 
     }
 }
