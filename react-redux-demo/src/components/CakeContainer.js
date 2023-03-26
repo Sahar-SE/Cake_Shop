0a6d52b6b9b3887597afe4b6727ec33aab1b862f
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-03-26 23:53:08 +0430

    Add props

--- react-redux-demo/src/components/CakeContainer.js
+++ react-redux-demo/src/components/CakeContainer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect} from 'react-redux'
 import  { buyCake } from '../redux'
 
-function CakeContainer() {
+function CakeContainer(props) {
   return (
     <div>
       <h2>Number of Cakes</h2>
