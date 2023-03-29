6856dd2473868bb39c5a7fc2b5b46baa5ab423c2
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-03-29 17:46:42 +0430

    Add provider

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux'
-import { store } from 'redux';
+import store from 'redux';
 import './App.css';
 import CakeContainer from './components/CakeContainer';
 
