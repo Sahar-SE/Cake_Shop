3222757c49eb8afc16f89b614d32100d7542d414
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-03-30 16:35:05 +0430

    Add useSelector in HooksCakeContainer

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -3,11 +3,13 @@ import { Provider } from 'react-redux'
 import store from './redux/store';
 import './App.css';
 import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <HooksCakeContainer></HooksCakeContainer>
         <CakeContainer />
       </div>
     </Provider>
