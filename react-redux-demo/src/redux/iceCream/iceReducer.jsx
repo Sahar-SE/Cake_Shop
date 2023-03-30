2df94bddf8bdd0dbc0e6d386041aa054e265697d
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-03-30 19:34:01 +0430

    Add action in reducer

--- react-redux-demo/src/redux/iceCream/iceReducer.jsx
+++ react-redux-demo/src/redux/iceCream/iceReducer.jsx
@@ -1,7 +1,13 @@
 import iceAction from "./iceActions";
+import BUY_ICECREAM from "./iceTypes";
 
 const initialState = {
     numOfIceCream: 10
 }
 
-const iceReducer = (state = initialState, action) => {}
+const iceReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case BUY_ICECREAM:
+            
+    }
+}
