162c29bd37dd8ad810e7706f030493332925c1ea
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-03-30 16:50:50 +0430

    Add useDispatch in HooksCakeContainer

--- react-redux-demo/src/components/HooksCakeContainer.jsx
+++ react-redux-demo/src/components/HooksCakeContainer.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
-import { BUY_CAKE } from '../redux/cake/cakeTypes'
 import { buyCake } from '../redux'
 
 
 function HooksCakeContainer() {
   const numOfCakes = useSelector(state => state.numOfCakes)
-  const buyCake = useDispatch(dispatch => buyCake)
+  const dispatch = useDispatch()
   return (
     <div>
       <h2>Number of Cakes- { numOfCakes }</h2>
-      <button onClick={buyCake}>BuyCake!</button>
+      <button onClick={() =>}>BuyCake!</button>
     </div>
   )
 }
