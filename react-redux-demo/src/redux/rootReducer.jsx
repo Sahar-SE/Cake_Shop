a229409022a8b87973ed7aa282367d3726101965
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-03-31 23:02:51 +0430

    Add root reducer

---
+++ react-redux-demo/src/redux/rootReducer.jsx
@@ -0,0 +1,11 @@
+import cakeReducer from "./cake/cakeReducer";
+// import iceReducer from "./iceCream/iceReducer";
+
+// const rootReducer = () => {
+//     return {
+//     cake: cakeReducer,
+//     iceCream: iceReducer
+//     }
+// }
+
+// export default rootReducer
