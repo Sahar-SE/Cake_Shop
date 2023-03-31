15cf05490f2b0afd1126b222061fe2746aa8e668
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-03-31 22:55:10 +0430

    Add iceReducer

--- react-redux-demo/src/redux/iceCream/iceReducer.jsx
+++ react-redux-demo/src/redux/iceCream/iceReducer.jsx
@@ -1,17 +1,17 @@
-import iceAction from "./iceActions";
-import BUY_ICECREAM from "./iceTypes";
+import { BUY_ICECREAM } from "./iceTypes";
 
-const initialState = {
-    numOfIceCream: 10
-}
+// const initialState = {
+//     numOfIceCreams: 10
+// }
 
-const iceReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case BUY_ICECREAM:
-            return {
-                ...state,
-                numOfIceCream: state.numOfIceCream - 1
-            }
-        default: return state
-    }
-}
+// const iceReducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case BUY_ICECREAM: return {
+//                 ...state,
+//                 numOfIceCreams: state.numOfIceCreams - 1
+//             }
+//         default: return state
+//     }
+// }
+
+// export default iceReducer
