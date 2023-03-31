848e722ea782c68bde67496d7432da9c1e9808af
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-03-31 23:11:11 +0430

    update app.js

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -4,13 +4,15 @@ import store from './redux/store';
 import './App.css';
 import CakeContainer from './components/CakeContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
+import IceCreamContainer from './components/IceCreamContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <HooksCakeContainer></HooksCakeContainer>
+        <HooksCakeContainer />
         <CakeContainer />
+        <IceCreamContainer />
       </div>
     </Provider>
   );
