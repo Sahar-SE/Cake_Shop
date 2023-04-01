68404e344952fc37e8f6506f88dc92e45f822d5c
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-04-01 19:39:40 +0430

    update iceContainer

--- react-redux-demo/src/components/IceCreamContainer.jsx
+++ react-redux-demo/src/components/IceCreamContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import buyIceCream from '../redux/iceCream/iceActions'
+import { buyIceCream } from '../redux'
 
 function IceCreamContainer(props) {
   return (
