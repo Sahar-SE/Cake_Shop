b5b6618ddef15c235b2791d8390015f5c48db2ba
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-04-02 23:02:01 +0430

    Add new cakeCintainer

--- react-redux-demo/src/components/newCakeContainer.jsx
+++ react-redux-demo/src/components/newCakeContainer.jsx
@@ -8,7 +8,7 @@ function newCakeContainer(props) {
     <div>
       <h2>Number of Cakes - {props.numOfCakes}</h2>
       <input type="text" value={number} onChange={e => setNumber(e.target.value)} />
-      <button onClick={() =>props.buyCake(number)}>Buy Cake</button>
+      <button onClick={() =>props.buyCake(number)}>Buy {number} Cake</button>
     </div>
   )
 }
